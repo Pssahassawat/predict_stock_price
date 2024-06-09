cab3a7eac3d3805fa97e81f3ef462da49b4d068d
Sahassawat Posungnern <89623103+Pssahassawat@users.noreply.github.com>
2024-06-09 16:34:48 +0700

    simple launch app

---
+++ predict_stock.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import yfinance as yf
+# from tensorflow.keras import load_model
+import streamlit as st
+
+start = '2010-01-01'
+# end = 'none'
+
+st.title('Stock Price Prediction')
+user_input = st.text_input('Enter a stock symbol')
+data = yf.download(user_input,start=start)
+
+st.subheader('Data from 2010 to now')
+st.write(data.describe())
